7a76573233dbe759f7e06e79d97368ceddbcc2f9
mgmartos <mgmartos@hotmail.com>
2021-11-09 11:54:03 +0100

    Añadimos clase Editoriales

---
+++ Prueba_Automapper/Models/Editoriales.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba_Automapper.Models
+{
+    public class Editoriales
+    {
+        public string Editorial { get; set; }
+        public string URL { get; set; }
+    }
+}
